31e883b4fae8c77b578f0b6d34366838759a82e9
Diego Niemeyer <102764313+dniemeryers@users.noreply.github.com>
2023-05-04 07:00:34 -0300

    Update Educação.tsx

--- src/page/educação/Educação.tsx
+++ src/page/educação/Educação.tsx
@@ -25,58 +25,8 @@ export function Educacao(){
                 <p>Concluído 2004 - 2006</p>
             </div>
         </div>
-            <h1>Cursos e BootCamp</h1>
-            <div className="certificados2">
-                <div className="curso" >
-                    <img className="certlinha1" src={ScrumMaster} alt="" />
-                    <h1>Scrum Master</h1>
-                    <h2>Dio</h2>                                      
-                </div>     
-                <div className="curso" >
-                    <img className="certlinha1" src={Scrum} alt="" />
-                    <h1>Certificação Scrum</h1>
-                    <h2>CertiProf</h2>                                      
-                </div>                          
-                <div className="curso" >
-                    <img className="certlinha1" src={Reactype} alt="" />
-                    <h1>React - TypeScript</h1>
-                    <h2>Workover</h2>                          
-                </div>  
-                <div className="curso" >
-                    <img className="certlinha1" src={JsWorkover} alt="" />
-                    <h1>JavaScripit </h1>
-                    <h2>Workover</h2>                         
-                </div>        
-                <div className="curso" >
-                    <img className="certlinha1" src={TypeScript} alt="" />
-                    <h1>TypeScript</h1>
-                    <h2>Workover</h2>                          
-                </div>  
-               
-                                                            
-            </div>
-            <div className="certificados2">
-                <div className="curso" >
-                        <img className="certlinha2" src={Code} alt="" />
-                        <h1>Desafios em JS</h1>
-                        <h2>Dio</h2>                          
-                </div>
-                <div className="curso" >
-                        <img className="certlinha2" src={Campus} alt="" />
-                        <h1>Campus Expert</h1>
-                        <h2>Dio</h2>                                      
-                </div>                          
-                <div className="curso" >
-                        <img className="certlinha2" src={Orbi} alt="" />
-                        <h1>Orbi Games</h1>
-                        <h2>Dio</h2>                          
-                </div>  
-                <div className="curso" >
-                        <img className="certlinha2" src={Orange} alt="" />
-                        <h1>Orange Tech</h1>
-                        <h2>Dio</h2>                         
-                </div>                                                          
-            </div>                   
+                                         
+                            
     </Container>
     </>)
 
